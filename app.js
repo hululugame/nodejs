aac3fed769e63e8cf002c8b50008ff95399a5637
hululugame <a0932926727@gmail.com>
2026-03-28 12:35:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log("🔥 APP STARTED");
   console.log(`Server running on port ${PORT}`);
 });
